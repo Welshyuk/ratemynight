bf6f52eef366c1a023a78a73926e0d0dca651d58
Welshyuk <welshy@welshy.me.uk>
2014-03-24 23:24:27 -0700

    test

--- index.php
+++ index.php
@@ -1,36 +1,71 @@
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US"
-      xmlns:fb="https://www.facebook.com/2008/fbml"> 
-<head>
-  <title>Rate My Night</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-</head>
-<body>
-
-<script src="http://code.jquery.com/jquery.js"></script>
-<script src="js/bootstrap.min.js"></script>
-
-<div class="container">
-
-<table class="table" >
-<tr class="success"  >
-
- <center>
- Been out tonight?<p>
- Was it good?<p>
- Choose your rating below - pew pew<p><p>
- <form action="rating.php" method="get">
- <input type="checkbox" name="awesome" value="Awesome" /> Awesome<br />
-<input type="checkbox" name="pass" value="Pass" /> Pass<br />
-<input type="checkbox" name="lame" value="lame" /> Lame<br />
-<input type="checkbox" name="fail" value="Fail" /> Fail<br /><p>
-<input type="submit" value="Submit" />
-</form>
-</center>
-</tr>
-</table>
-</div>
-
-</body>
-</html>
+    </head>
+	<body>
+	<style type="text/css">
+	<body>
+	</style>
+	 <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	<div class="container">
+    <p class="muted">Fusce dapibus, tellus ac cursus commodo, tortor mauris nibh.</p>
+    <p class="text-warning">Etiam porta sem malesuada magna mollis euismod.</p>
+    <p class="text-error">Donec ullamcorper nulla non metus auctor fringilla.</p>
+    <p class="text-info">Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis.</p>
+    <p class="text-success">Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+	
+	<table class="table" >
+	<tr class="success"  >
+    <td>1</td>
+    <td>01/04/2012</td>
+	<td>100.00</td>
+	<td>7.00</td>
+	<td>107.00</td>
+    <td>Approved</td>
+  </tr>
+  
+  <tr class="error">
+    <td>2</td>
+    <td>01/10/2012</td>
+	<td>100.00</td>
+	<td>7.00</td>
+	<td>107.00</td>
+    <td>Declined</td>
+  </tr>
+  
+   <tr class="error">
+    <td>3</td>
+    <td>01/10/2012</td>
+	<td>50.00</td>
+	<td>3.50</td>
+	<td>53.50</td>
+    <td>50% Declined</td>
+  </tr>
+  
+  <tr class="error">
+    <td>4</td>
+    <td>01/15/2012</td>
+	<td>100.00</td>
+	<td>7.00</td>
+	<td>107.00</td>
+    <td>Declined</td>
+  </tr>
+  
+  <tr class="success">
+    <td>5</td>
+    <td>01/15/2012</td>
+	<td>50.00</td>
+	<td>3.50</td>
+	<td>53.50</td>
+    <td>50% Approved</td>
+  </tr>
+  
+  </table>
+	</div>
+    </body>
+    </html>
