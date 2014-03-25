6af3da1103b319183aa7e870dc92a6d3cb8903d8
Welshyuk <welshy@welshy.me.uk>
2014-03-24 22:59:22 -0700

    bootstrap install

--- index.php
+++ index.php
@@ -2,8 +2,14 @@
       xmlns:fb="https://www.facebook.com/2008/fbml"> 
 <head>
   <title>Rate My Night</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 </head>
 <body>
+
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
 
  <center>
  Been out tonight?<p>
