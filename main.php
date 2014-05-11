15be96774d0559d8287d21318224c9f374e9d3c0
Welshyuk <welshy@welshy.me.uk>
2014-05-10 23:01:54 -0700

    i got this

--- main.php
+++ main.php
@@ -14,36 +14,43 @@ a {color:#04B486;}
 
 </style>
 
-<div class="page-header">
-   <h2>Rate My Night
-      <small>Been out tonight?</small>
-   </h2>
+<table>
+<td>
+</br>
+</td>
+	<td>
+	<div class="span8 offset2">
+
+		<div class="page-header">
+		   <h2>Rate My Night
+			  <small>Been out tonight?</small>
+		   </h2>
+		</div>
+
+		<ul class="nav nav-tabs nav-justified" id="myTab">
+		   <li class="active"><a href="home" data-target="#home" data-toggle="tab">Home</a></li>
+		   <li><a href="recent" data-target="#recent" data-toggle="tab">My Rates</a></li>
+		   <li><a href="settings" data-target="#settings" data-toggle="tab">Settings</a></li>
+		</ul>
+
+		<div class="tab-content">
+		  <div class="tab-pane active" id="home">..1.</div>
+		  <div class="tab-pane" id="recent">.2..</div>
+		  <div class="tab-pane" id="settings">.3..</div>
+		</div>
+
+		<script>
+		  jQuery(function () {
+			jQuery('#myTab a').on('click', function() {
+				$(this).tab('show');
+			});
+		})
+				
+				
+		</script>
 </div>
-
-<ul class="nav nav-tabs nav-justified" id="myTab">
-   <li class="active"><a href="home" data-target="#home" data-toggle="tab">Home</a></li>
-   <li><a href="recent" data-target="#recent" data-toggle="tab">My Rates</a></li>
-   <li><a href="settings" data-target="#settings" data-toggle="tab">Settings</a></li>
-</ul>
-
-
-
-
-<div class="tab-content">
-  <div class="tab-pane active" id="home">..1.</div>
-  <div class="tab-pane" id="recent">.2..</div>
-  <div class="tab-pane" id="settings">.3..</div>
-</div>
-
-<script>
-  jQuery(function () {
-    jQuery('#myTab a').on('click', function() {
-        $(this).tab('show');
-    });
-})
-		
-		
-</script>
+	</td>
+	</td>
 </head>
 <body>
 
