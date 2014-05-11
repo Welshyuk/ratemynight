85e9ba5b6254997e7767b7a9beab7660f5bf83a7
Welshyuk <welshy@welshy.me.uk>
2014-05-10 22:52:55 -0700

    working

--- main.php
+++ main.php
@@ -4,22 +4,46 @@
   <title>Rate My Night</title>
   <!-- Bootstrap -->
 <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="css/site.css" rel="stylesheet">
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+ <script src="js/bootstrap-tabs.js"></script>
+
+<style>
+a {color:#04B486;}
+
+</style>
+
+<div class="page-header">
+   <h2>Rate My Night
+      <small>Been out tonight?</small>
+   </h2>
+</div>
+
+<ul class="nav nav-tabs nav-justified" id="myTab">
+   <li class="active"><a href="#home" data-target="home" data-toggle="tab">Home</a></li>
+   <li><a href="#recent" data-target="recent" data-toggle="tab">My Rates</a></li>
+   <li><a href="#settings" data-target="settings" data-toggle="tab">Settings</a></li>
+</ul>
+
 </head>
 <body>
 
-<p>
- <center>
- Been out tonight?<p>
- Was it good?<p>
- Choose your rating below<p><p>
- <form action="rating.php" method="get">
- <input type="checkbox" name="awesome" value="Awesome" /> Awesome<br />
-<input type="checkbox" name="pass" value="Pass" /> Pass<br />
-<input type="checkbox" name="lame" value="lame" /> Lame<br />
-<input type="checkbox" name="fail" value="Fail" /> Fail<br /><p>
-<input type="submit" value="Submit" />
-</form>
-</center>
+<div class="tab-content">
+  <div class="tab-pane active" id="home">..1.</div>
+  <div class="tab-pane" id="recent">.2..</div>
+  <div class="tab-pane" id="settings">.3..</div>
+</div>
+
+<script>
+  jQuery(function () {
+    jQuery('#myTab a').on('click', function() {
+        $(this).tab('show');
+    });
+})
+		
+		
+</script>
 
 </body>
 </html>
